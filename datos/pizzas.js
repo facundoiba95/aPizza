a5974842f9b8d402b36309fe97f7d59dcd162ad9
facundoiba95 <facundoiba95@gmail.com>
2022-10-06 15:54:19 -0300

    inicio de proyecto

---
+++ datos/pizzas.js
@@ -0,0 +1,40 @@
+let Pizzas = [
+    {id:1,
+     nombre:"simple",
+     ingredientes: ['queso','tomate','oregano'],
+     precio: 500,
+    },
+  
+    {id:2,
+      nombre:"completa",
+      ingredientes: ['queso','jamon','anchoas','salsa','aceitunas','oregano'],
+      precio: 1000,
+    },
+  
+    {id:3,
+     nombre:"especial",
+     ingredientes: ['muza','salsa','morron','jamon','aceitunas','oregano'],
+     precio: 1300,
+    },
+  
+    {id:4,
+      nombre:"rucula",
+      ingredientes: ['queso','salsa','jamon','rucula'],
+      precio: 1400,
+    },
+  
+    {id:5,
+      nombre:"palmitos",
+      ingredientes: ['queso','salsa','morron','jamon','palmitos'],
+      precio: 1300,
+    },
+  
+    {id:6,
+      nombre:"muza",
+      ingredientes: ['muza','salsa','morron','oregano'],
+      precio: 800,
+    },
+  ]
+  
+  module.exports= Pizzas;
+  
---
+++ router.js
@@ -0,0 +1,26 @@
+const http = require('http')
+const express = require('express');
+const app = express();
+
+const Pizzas = require('./datos/pizzas');
+// const routerPizzas = express.Router();
+// app.use('/api', routerPizzas)
+
+
+// routerPizzas.get('/',(req,res)=> {
+//     return res.send(JSON.stringify(Pizzas))
+// })
+app.get('/',(req,res)=> {
+    const path = req.path;
+    if(path === '/'){
+       return res.send(JSON.stringify(Pizzas))
+    } else {
+        return res.status(404).send('No se encontro la pagina :/')
+    }
+})
+
+
+const PUERTO = process.env.PORT || 3001;
+app.listen(PUERTO, ()=> {
+    console.log(`El servidor esta escuchando en el puerto: ${PUERTO}`)
+})
