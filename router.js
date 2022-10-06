ddb9a82d26bf40637d65d8bea65d0670e70b3bb0
facundoiba95 <facundoiba95@gmail.com>
2022-10-06 17:23:01 -0300

    correcion cors

--- router.js
+++ router.js
@@ -5,6 +5,13 @@ const app = express();
 const Pizzas = require('./datos/pizzas')
 const routerPizzas = express.Router();
 app.use('/api/pizzas', routerPizzas)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 
 // routerPizzas.get('/',(req,res)=> {
